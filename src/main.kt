11bd7a45477df1eb871307800403e12cfa012e5a
Paulo Roberto <paulo.almeida@wisereducacao.com>
2020-07-25 17:42:41 -0300

    Restrições das variáveis
    Aprendendo a como tipar a variável e a como a atribuição de um novo valor depende do valor já existente na variavel.

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,9 @@
 fun main() {
     val titular = "Anthony Roberto"
-
+    val numeroConta = "1000"
+    var saldo = 0.0
+    saldo += 200
     println("O nome do titular é: $titular")
+    println("O numero da conta é: $numeroConta")
+    println("O saldo é: $saldo")
 }
