0c9f23a0d124c9845c7ef6fb1ccffce603dc254d
Paulo Roberto <paulo.almeida@wisereducacao.com>
2020-07-26 17:19:53 -0300

    Controle de fluxo
    Testanto if e when:

--- src/main.kt
+++ src/main.kt
@@ -2,8 +2,22 @@ fun main() {
     val titular = "Anthony Roberto"
     val numeroConta = "1000"
     var saldo = 0.0
-    saldo += 200
+    saldo = 5.0
     println("O nome do titular é: $titular")
     println("O numero da conta é: $numeroConta")
     println("O saldo é: $saldo")
+
+//    if (saldo > 0.0) {
+//        println("Saldo positivo")
+//    } else if (saldo == 0.0) {
+//        println("Saldo neutro")
+//    } else {
+//        println("Saldo negativo")
+//    }
+//
+    when {
+        saldo > 0.0 -> println("Saldo positivo")
+        saldo === 0.0 -> println("Saldo neutro")
+        else -> println("Saldo negativo")
+    }
 }
